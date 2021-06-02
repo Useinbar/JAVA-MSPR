dd8e3bed3294f4bce67071899258014baa744bf0
Spykou <69178217+SpykouKera@users.noreply.github.com>
2021-06-02 13:15:47 +0200

    update de la modification de index.html pour ajouter les agents et liens hypertextes.

--- src/index.java
+++ src/index.java
@@ -1,25 +1,48 @@
+import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.nio.file.*;
-import java.util.*;
+import java.util.ArrayList;
+
 
 public class index {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
 
+/*** On copie le index-static.html, qui est le "corps" de l'index html dans le fichier index.html.
+ */
         Path indexStaticPath = Paths.get("html/index-static.html");
         Path indexPath = Paths.get("html/index.html");
-
-        if (Files.exists(indexStaticPath)) {
-            System.out.println("Le fichier existe");
-
+        try {
             Files.copy(indexStaticPath, indexPath,StandardCopyOption.REPLACE_EXISTING);
-
-            List<String> lines = null;
-            try {
-                lines = Files.readAllLines(indexStaticPath);
-                System.out.println(lines);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-            }
+        } catch (IOException ex) {
+            System.out.println("Le fichier index-static n'a pas été trouvé");
         }
+/**
+ * Création du String qui affichera la liste des agents + les liens hypertextes à insérer dans la page html.index
+ */
+
+
+       String stringAgents="Kenny Rapina";
+
+
+/*** On convertit le html en File, on copie le contenu de index.html dans oldcontent.
+ * Puis on remplace le BLOC_AGENTS par la liste des agents dans un String newContent.
+ * Enfin, on balance le newContent dans le fichier index.html.
+ */
+        File indexFile= indexPath.toFile();
+        String oldContent = "";
+        BufferedReader reader = new BufferedReader(new FileReader(indexFile));
+        String line = reader.readLine();
+        while (line != null)
+        {
+            oldContent = oldContent + line + System.lineSeparator();
+            line = reader.readLine();
+        }
+        String newContent = oldContent.replaceAll("BLOC_AGENTS", stringAgents);
+        FileWriter writer = new FileWriter(indexFile);
+        writer.write(newContent);
+        reader.close();
+        writer.close();
+
     }
+}
